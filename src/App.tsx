7f459344d3b0a4c6b890cbdfa657764dc636b172
TalBenZvi <talbz03@gmail.com>
2022-04-06 20:37:54 +0300

    got peer-to-peer to work

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,154 @@ import "./App.css";
 //import { ClientFlowEngine } from "./client_side/client_flow_engine";
 //import { ServerFlowEngine } from "./server_side/server_flow_engine";
 
+/*
+import { useEffect, useState, useReducer } from 'react'
+import Gun from 'gun'
+
+// initialize gun locally
+const gun = Gun({
+  peers: [
+    'http://localhost:3030/gun'
+  ]
+})
+
+// create the initial state to hold the messages
+const initialState = {
+  messages: []
+}
+
+// Create a reducer that will update the messages array
+function reducer(state: any, message: any) {
+  return {
+    messages: [message, ...state.messages]
+  }
+}
+*/
+
+import TestComponent from "./components/test_component"
+
+export default function App() {
+  
+
+  
+
+
+  return <TestComponent />;
+
+  /*
+  // the form state manages the form input for creating a new message
+    const [formState, setForm] = useState({
+    name: '', message: ''
+  })
+
+  // initialize the reducer & state for holding the messages array
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // when the app loads, fetch the current messages and load them into the state
+  // this also subscribes to new data as it changes and updates the local state
+  useEffect(() => {
+    const messages = gun.get('messages')
+    messages.map().on(m => {
+      dispatch({
+        name: m.name,
+        message: m.message,
+        createdAt: m.createdAt
+      })
+    })
+  }, [])
+
+  // set a new message in gun, update the local state to reset the form field
+  function saveMessage() {
+    const messages = gun.get('messages')
+    messages.set({
+      name: formState.name,
+      message: formState.message,
+      createdAt: Date.now()
+    })
+    setForm({
+      name: '', message: ''
+    })
+  }
+
+  // update the form state as the user types
+  function onChange(e: any) {
+    setForm({ ...formState, [e.target.name]: e.target.value  })
+  }
+
+  return (
+    <div style={{ padding: 30 }}>
+      <input
+        onChange={onChange}
+        placeholder="Name"
+        name="name"
+        value={formState.name}
+      />
+      <input
+        onChange={onChange}
+        placeholder="Message"
+        name="message"
+        value={formState.message}
+      />
+      <button onClick={saveMessage}>Send Message</button>
+      {
+        state.messages.map(message => (
+          <div key={message.createdAt}>
+            <h2>{message.message}</h2>
+            <h3>From: {message.name}</h3>
+            <p>Date: {message.createdAt}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
+  */
+}
+
+/*
 function App() {
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /*
   let serverFlowEngine: ServerFlowEngine = new ServerFlowEngine();
   let clientFlowEngines: ClientFlowEngine[] = [...Array(NUM_OF_PLAYERS)].map(
     (_, i) => new ClientFlowEngine()
   );
   */
- return <h1>hello</h1>
- /*
+//return <h1>hello</h1>
+/*
   return (
     <div className="centered">
       <BoardComponent
@@ -36,7 +175,8 @@ function App() {
       </button>
     </div>
   );
-  */
+  
 }
 
 export default App;
+*/
--- src/components/test_component.tsx
+++ src/components/test_component.tsx
@@ -1,36 +1,83 @@
-import { getFormLabelUtilityClasses } from '@mui/material';
-import React, { Component } from 'react'
-import { Button, Dimmer, Header, Icon } from 'semantic-ui-react'
-  
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = 
-"https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
-  
+import React, { Component } from "react";
+
+import Gun from "gun";
+
 export default class TestComponent extends Component {
-  state = {dim: false}
-  
-  GetDIM = () => this.setState({ dim: true })
-  HideDIM = () => this.setState({ dim: false })
-  
+  gun: any;
+  david: any;
+  queryGun = null
+  state = { data: 0, result: "" };
+
+  constructor(props: any) {
+    super(props);
+    this.gun = Gun({
+      peers: ["http://localhost:3030/gun"],
+    });
+
+    let people = this.gun.get("people");
+
+    people.set({ name: "jeff7" });
+
+    this.david = this.gun.get("david").set({ id: 0, name: "david4" });
+
+    //this.queryGun = this.gun.get("newQuery").set({ data: "zoe0" });
+
+  }
+
   render() {
-    const { dim } = this.state
-  
     return (
       <div>
-        <Dimmer.Dimmable dimmed={true}>
-          <Header as='h3'>GeeksforGeeks </Header>
-          Semantic UI
-          
-          <br />
-          <Icon name='react' size='huge' />
-          <Dimmer active={true} />
-        </Dimmer.Dimmable>
-  
-        <Button onClick={this.GetDIM} content='Dim' />
-        <Button onClick={this.HideDIM} content='Original' />
+        <p>test</p>
+        <p>{`data: ${this.state.data}`}</p>
+        <button
+          onClick={() => {
+            this.setState(() => {
+              return { data: this.state.data + 1 };
+            });
+          }}
+        >
+          increment
+        </button>
+        <button
+          onClick={() => {
+            this.setState(() => {
+              return { data: this.state.data - 1 };
+            });
+          }}
+        >
+          decrement
+        </button>
+        <button
+          onClick={() => {
+            let result = "";
+            let i = 0;
+            /*
+            (this.queryGun as any).once((data: any) => {
+              //console.log(i.toString() + " " + JSON.stringify(data.name));
+              i++;
+              this.setState(() => {
+                return { result: data.name };
+              });
+            });
+            */
+            this.gun.get("newerQuery").map().once((result: any) => {
+              this.setState(() => {
+                return {result: result.data}
+              })
+            })
+          }}
+        >
+          query
+        </button>
+        <button
+          onClick={() => {
+            this.gun.get("newerQuery").set({ data: this.state.data });
+          }}
+        >
+          send data
+        </button>
+        <p>{this.state.result}</p>
       </div>
-    )
+    );
   }
 }
