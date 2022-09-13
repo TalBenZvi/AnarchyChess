e8d456d74d1c0ea74119ed212b9ac3d74de93463
TalBenZvi <talbz03@gmail.com>
2022-09-13 11:48:09 +0300

    moving to https

--- express_server/server.js
+++ express_server/server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+var https = require('https');
+var privateKey  = fs.readFileSync('../deployment/private_key.pem', 'utf8');
+var certificate = fs.readFileSync('../deployment/anarchy_chess.xyz', 'utf8');
 
 app.use(express.static(path.join(__dirname, "../build")));
-
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../build/index.html"));
 })
 
+var httpsServer = https.createServer({key: privateKey, cert: certificate}, app);
 
-app.listen(3000, () => {
+httpsServer.listen(3000, () => {
     console.log("server listening on port 3000");
 })
