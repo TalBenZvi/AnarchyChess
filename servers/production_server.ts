2b5657f8677c532cba50711db29235b512eec653
TalBenZvi <talbz03@gmail.com>
2022-10-03 22:30:59 +0300

    production server now redirects http to https

--- servers/production_server.ts
+++ servers/production_server.ts
@@ -6,6 +6,7 @@ import * as path from "path";
 // https
 import * as fs from "fs";
 import * as https from "https";
+import * as http from "http";
 
 import { fileURLToPath } from "url";
 // @ts-ignore
@@ -23,24 +24,35 @@ const certificate = fs.readFileSync(
   "utf8"
 );
 
+app.enable("trust proxy");
+app.use((req, res, next) => {
+  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
 app.use(express.static(path.join(__dirname, "../../build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../build/index.html"));
 });
 
-app.get('*', function(req, res) {
-  res.redirect('/');
+app.get("*", function (req, res) {
+  res.redirect("/");
 });
-
 
 const httpsServer = https.createServer(
   { key: privateKey, cert: certificate },
   app
 );
 
-httpsServer.listen(3031, () => {
-  console.log("server listening on port 3031");
+httpsServer.listen(3443, "0.0.0.0", () => {
+  console.log("server listening on port 3443");
 });
+
+const httpToHttps = http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+});
+
+httpToHttps.listen(3080);
 
 const appServer = new AppServer(httpsServer);
